959fec5249d15906ff5649cc54ef4c4862511d50
bkilrain <bkilrain@gmail.com>
2016-07-25 15:59:27 -0700

    Implement stringifyJSON functionaity and cleanup test spec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var result;
+  var type = typeof obj;
+
+  if (type === 'boolean' || type === 'number' || obj === null) {
+    result = '' + obj;
+  } else if (type === 'string') {
+    result = '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    var arr = [];
+    for(var i = 0;i<obj.length;i++){
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '['+ arr.join(',') + ']';
+  } else if (type === 'object'){
+    var objArray = [];
+    for(var key in obj){
+      if(obj[key] === undefined || typeof obj[key] === 'function'){
+        continue;
+      }
+      var keyString = stringifyJSON(key);
+      var valString = stringifyJSON(obj[key]);
+      objArray.push(keyString + ":" + valString); 
+    }
+    return '{'+ objArray.join(',') + '}';
+  }
+
+  return result;
+
 };
